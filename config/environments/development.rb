c17d220231288988cae5a7e03da387fb3e99a66a
sou1016 <soudai311016@ezweb.ne.jp>
2022-04-09 08:42:13 +0000

    top作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
+config.hosts << "3b6c750a6e074ecfa10719f76b852614.vfs.cloud9.us-east-1.amazonaws.com"
 end
